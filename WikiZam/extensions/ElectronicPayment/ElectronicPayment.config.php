b4d4dba37621f68972a033c48adbddaddd336726
bedhed <clement@seizam.com>
2012-01-11 17:38:29 +0100

    path to CMCIC stubs fixed

--- WikiZam/extensions/ElectronicPayment/ElectronicPayment.config.php
+++ WikiZam/extensions/ElectronicPayment/ElectronicPayment.config.php
@@ -2,4 +2,4 @@
 // TPE Settings
 // Warning !! CMCIC_Config contains the key, you have to protect this file with all the mechanism available in your development environment.
 // You may for instance put this file in another directory and/or change its name.
-require_once("CMCIC_Config.php");
+require_once(dirname(__FILE__)."/CMCIC_Config.php");
