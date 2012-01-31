e276f23f8d2ab72ea684e3916cdc09b5c7ec992a
bedhed <clement@seizam.com>
2012-01-31 17:34:22 +0100

    Electronic Payment i18n

--- WikiZam/extensions/ElectronicPayment/ElectronicPayment.i18n.php
+++ WikiZam/extensions/ElectronicPayment/ElectronicPayment.i18n.php
@@ -16,13 +16,10 @@ $messages['en'] = array(
 	'electronicpayment-desc'=>'Virtual Electronic Payment Terminal for Seizam',
         'eptback' => 'EPTBack',
         'ep-action' => 'Please click on the button below to reach our bank (CIC) secure electronic payment interface. Transaction amount: \'\'\'$1\'\'\'',
-        'ep-fail' => 'The transaction failed. Try again.',
+        'ep-fail' => 'The transaction failed. Sorry.',
         'ep-success' => 'The transaction succeeded. \o/',
         'ep-connect' => 'Connect',
-        'ep-fd-ordersection' => 'My Order',
-        'ep-fd-personalsection' => 'My Details',
-        'ep-fd-amountlabel' => 'Amount in Euro (€)',
-        'ep-fd-emaillabel' => 'Email for order confirmation'
+        'ep-fd-amountlabel' => 'Amount in Euros (€)'
         
     
 );
@@ -41,7 +38,8 @@ $messages['fr'] = array(
         'electronicpayment' => 'Paiement Électronique',
 	'electronicpayment-desc' => 'Terminal de Paiement Électronique Virtuel pour Seizam',
         'ep-action' => 'Cliquez sur le bouton ci-dessous pour atteindre l\'interface de paiement sécurisée de notre banque (CIC). Montant de la transaction : \'\'\'$1\'\'\'',
-        'ep-fail' => 'La transaction est ratée. Essayez encore.',
+        'ep-fail' => 'La transaction est ratée. Désolé.',
         'ep-success' => 'La transaction est réussie. \o/',
-        'ep-connect' => 'Connection'
+        'ep-connect' => 'Connexion',
+        'ep-fd-amountlabel' => 'Montant en Euros (€)'
 );
