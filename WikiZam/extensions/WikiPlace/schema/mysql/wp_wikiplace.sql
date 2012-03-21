5dcbc1e0752634bd06eaab542a15bf60e7f455ac
Yannouk <yann.missler@gmail.com>
2012-03-21 18:05:45 +0100

    updated wikiplace

--- WikiZam/extensions/WikiPlace/schema/mysql/wp_wikiplace.sql
+++ WikiZam/extensions/WikiPlace/schema/mysql/wp_wikiplace.sql
@@ -10,6 +10,6 @@ CREATE TABLE IF NOT EXISTS `wp_wikiplace` (
   `wpw_owner_user_id` int(10) unsigned NOT NULL COMMENT 'Foreign key: the user who owns the WikiPlace',
   `wpw_home_page_id` int(10) unsigned NOT NULL COMMENT 'WikiPlace homepage WpPage identifier',
   PRIMARY KEY (`wpw_id`),
-  UNIQUE KEY `wpw_home_wppa_id` (`wpw_home_wppa_id`),
+  UNIQUE KEY `wpw_home_page_id` (`wpw_home_page_id`),
   KEY `wpw_owner_user_id` (`wpw_owner_user_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=binary;
