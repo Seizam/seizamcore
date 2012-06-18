aa1da52ef689696d9159d1a1f1ffe8699134576e
bedhed <clement@seizam.com>
2012-06-18 16:40:30 +0200

    Widget:AddThis CSS

--- WikiZam/extensions/Widgets/compiled_templates/%%AF^AF6^AF67916D%%wiki%3AAddThis.php
+++ WikiZam/extensions/Widgets/compiled_templates/%%AF^AF6^AF67916D%%wiki%3AAddThis.php
@@ -1,10 +1,10 @@
-<?php /* Smarty version 2.6.18-dev, created on 2012-06-15 17:17:04
+<?php /* Smarty version 2.6.18-dev, created on 2012-06-18 16:31:55
          compiled from wiki:AddThis */ ?>
 <?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
 smarty_core_load_plugins(array('plugins' => array(array('function', 'counter', 'wiki:AddThis', 13, false),array('modifier', 'escape', 'wiki:AddThis', 13, false),array('modifier', 'default', 'wiki:AddThis', 13, false),)), $this); ?>
 
 
-<div class="addthis_toolbox addthis_default_style<?php if (isset ( $this->_tpl_vars['big'] )): ?> addthis_32x32_style<?php else: ?> addthis_16x16_style<?php endif; ?><?php if (isset ( $this->_tpl_vars['right'] )): ?> right<?php elseif (isset ( $this->_tpl_vars['left'] )): ?> left<?php endif; ?>">
+<div class="addthis_toolbox addthis_default_style<?php if (isset ( $this->_tpl_vars['big'] )): ?> addthis_32x32_style<?php else: ?> addthis_16x16_style<?php endif; ?><?php if (isset ( $this->_tpl_vars['right'] )): ?> right<?php elseif (isset ( $this->_tpl_vars['left'] )): ?> left<?php endif; ?><?php if (isset ( $this->_tpl_vars['vertical'] )): ?> vertical<?php else: ?><?php if (isset ( $this->_tpl_vars['long'] )): ?> long<?php endif; ?><?php if (isset ( $this->_tpl_vars['counter'] )): ?> counter<?php endif; ?><?php endif; ?>">
 <a class="addthis_button_preferred_1"></a>
 <a class="addthis_button_preferred_2"></a>
 <?php if (isset ( $this->_tpl_vars['long'] )): ?>
@@ -12,8 +12,8 @@ smarty_core_load_plugins(array('plugins' => array(array('function', 'counter', '
 <a class="addthis_button_preferred_4"></a>
 <?php endif; ?>
 <a class="addthis_button_compact"></a>
-<?php if (isset ( $this->_tpl_vars['counter'] )): ?><a class="addthis_counter addthis_bubble_style"></a><?php endif; ?>
+<?php if (isset ( $this->_tpl_vars['counter'] ) && ! isset ( $this->_tpl_vars['vertical'] )): ?><a class="addthis_counter addthis_bubble_style"></a><?php endif; ?>
 </div>
 <?php echo smarty_function_counter(array('name' => 'addthiscounter','assign' => 'addthisincluded'), $this);?>
-<?php if ($this->_tpl_vars['addthisincluded'] == 1): ?><script type="text/javascript">var addthis_config={data_ga_property:'UA-32666889-1',data_ga_social:true};</script><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=<?php echo ((is_array($_tmp=((is_array($_tmp=$this->_tpl_vars['pubid'])) ? $this->_run_mod_handler('escape', true, $_tmp, 'urlpathinfo') : smarty_modifier_escape($_tmp, 'urlpathinfo')))) ? $this->_run_mod_handler('default', true, $_tmp, 'ra-4fdafa43072e511d') : smarty_modifier_default($_tmp, 'ra-4fdafa43072e511d')); ?>
+<?php if ($this->_tpl_vars['addthisincluded'] == 1): ?><script type="text/javascript">var addthis_config={data_ga_property:'UA-32666889-1',data_ga_social:true,services_exclude:'print'};</script><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=<?php echo ((is_array($_tmp=((is_array($_tmp=$this->_tpl_vars['pubid'])) ? $this->_run_mod_handler('escape', true, $_tmp, 'urlpathinfo') : smarty_modifier_escape($_tmp, 'urlpathinfo')))) ? $this->_run_mod_handler('default', true, $_tmp, 'ra-4fdafa43072e511d') : smarty_modifier_default($_tmp, 'ra-4fdafa43072e511d')); ?>
 "></script><?php endif; ?>
